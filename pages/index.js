adf98fcda06f2b19ae33b6c7e963c82e81acdbbf
Akasshhhh <akashisntreal@gmail.com>
2023-12-28 21:04:24 +0530

    useEffect issues

--- pages/index.js
+++ pages/index.js
@@ -21,55 +21,25 @@ export default function Home() {
 
   const [valueOfCoin, setCoin] = useState("RUSD");
 
-  /** General state variables */
-  // loading is set to true when the transaction is mining and set to false when
-  // the transaction has mined
   const [loading, setLoading] = useState(false);
-  // We have two tabs in this dapp, Liquidity Tab and Swap Tab. This variable
-  // keeps track of which Tab the user is on. If it is set to true this means
-  // that the user is on `liquidity` tab else he is on `swap` tab
+
   const [liquidityTab, setLiquidityTab] = useState(true);
-  // This variable is the `0` number in form of a BigNumber
   const zero = BigNumber.from(0);
-  /** Variables to keep track of amount */
-  // `ethBalance` keeps track of the amount of Eth held by the user's account
   const [ethBalance, setEtherBalance] = useState(zero);
-  // `reservedCD` keeps track of the Crypto Dev tokens Reserve balance in the Exchange contract
   const [reservedCD, setReservedCD] = useState(zero);
-  // Keeps track of the ether balance in the contract
   const [etherBalanceContract, setEtherBalanceContract] = useState(zero);
-  // cdBalance is the amount of `CD` tokens help by the users account
   const [cdBalance, setCDBalance] = useState(zero);
-  // `lpBalance` is the amount of LP tokens held by the users account
   const [lpBalance, setLPBalance] = useState(zero);
-  /** Variables to keep track of liquidity to be added or removed */
-  // addEther is the amount of Ether that the user wants to add to the liquidity
   const [addEther, setAddEther] = useState(zero);
-  // addCDTokens keeps track of the amount of CD tokens that the user wants to add to the liquidity
-  // in case when there is no initial liquidity and after liquidity gets added it keeps track of the
-  // CD tokens that the user can add given a certain amount of ether
   const [addCDTokens, setAddCDTokens] = useState(zero);
-  // removeEther is the amount of `Ether` that would be sent back to the user based on a certain number of `LP` tokens
   const [removeEther, setRemoveEther] = useState(zero);
-  // removeCD is the amount of `Crypto Dev` tokens that would be sent back to the user based on a certain number of `LP` tokens
-  // that he wants to withdraw
   const [removeCD, setRemoveCD] = useState(zero);
-  // amount of LP tokens that the user wants to remove from liquidity
   const [removeLPTokens, setRemoveLPTokens] = useState("0");
-  /** Variables to keep track of swap functionality */
-  // Amount that the user wants to swap
-  const [swapAmount, setSwapAmount] = useState("");
-  // This keeps track of the number of tokens that the user would receive after a swap completes
-  const [tokenToBeReceivedAfterSwap, settokenToBeReceivedAfterSwap] = useState(
-    zero
-  );
-  // Keeps track of whether  `Eth` or `Crypto Dev` token is selected. If `Eth` is selected it means that the user
-  // wants to swap some `Eth` for some `Crypto Dev` tokens and vice versa if `Eth` is not selected
+
+
   const [ethSelected, setEthSelected] = useState(true);
   /** Wallet connection */
-  // Create a reference to the Web3 Modal (used for connecting to Metamask) which persists as long as the page is open
   const web3ModalRef = useRef();
-  // walletConnected keep track of whether the user's wallet is connected or not
   const [walletConnected, setWalletConnected] = useState(false);
 
   /**
@@ -102,29 +72,6 @@ export default function Home() {
     }
   };
 
-  /**** SWAP FUNCTIONS ****/
-
-  /**
-   * swapTokens: Swaps  `swapAmountWei` of Eth/Crypto Dev tokens with `tokenToBeReceivedAfterSwap` amount of Eth/Crypto Dev tokens.
-   */
-
-  /**
-   * _getAmountOfTokensReceivedFromSwap:  Returns the number of Eth/Crypto Dev tokens that can be received 
-   * when the user swaps `_swapAmountWEI` amount of Eth/Crypto Dev tokens.
-   */
-
-
-  /*** END ***/
-
-  /**** ADD LIQUIDITY FUNCTIONS ****/
-
-  /**
-   * _addLiquidity helps add liquidity to the exchange,
-   * If the user is adding initial liquidity, user decides the ether and CD tokens he wants to add
-   * to the exchange. If he is adding the liquidity after the initial liquidity has already been added
-   * then we calculate the crypto dev tokens he can add, given the Eth he wants to add by keeping the ratios
-   * constant
-   */
   const _addLiquidity = async () => {
     try {
       // Convert the ether amount entered by the user to Bignumber
@@ -150,14 +97,6 @@ export default function Home() {
     }
   };
 
-  /**** END ****/
-
-  /**** REMOVE LIQUIDITY FUNCTIONS ****/
-
-  /**
-   * _removeLiquidity: Removes the `removeLPTokensWei` amount of LP tokens from
-   * liquidity and also the calculated amount of `ether` and `CD` tokens
-   */
   const _removeLiquidity = async () => {
     try {
       const signer = await getProviderOrSigner(true);
@@ -178,11 +117,6 @@ export default function Home() {
     }
   };
 
-  /**
-   * _getTokensAfterRemove: Calculates the amount of `Ether` and `CD` tokens
-   * that would be returned back to user after he removes `removeLPTokenWei` amount
-   * of LP tokens from the contract
-   */
   const _getTokensAfterRemove = async (_removeLPTokens) => {
     try {
       const provider = await getProviderOrSigner();
@@ -206,15 +140,8 @@ export default function Home() {
     }
   };
 
-  /**** END ****/
-
-  /**
-   * connectWallet: Connects the MetaMask wallet
-   */
   const connectWallet = async () => {
     try {
-      // Get the provider from web3Modal, which in our case is MetaMask
-      // When used for the first time, it prompts the user to connect their wallet
       await getProviderOrSigner();
       setWalletConnected(true);
     } catch (err) {
@@ -222,29 +149,10 @@ export default function Home() {
     }
   };
 
-  /**
-   * Returns a Provider or Signer object representing the Ethereum RPC with or
-   * without the signing capabilities of Metamask attached
-   *
-   * A `Provider` is needed to interact with the blockchain - reading
-   * transactions, reading balances, reading state, etc.
-   *
-   * A `Signer` is a special type of Provider used in case a `write` transaction
-   * needs to be made to the blockchain, which involves the connected account
-   * needing to make a digital signature to authorize the transaction being
-   * sent. Metamask exposes a Signer API to allow your website to request
-   * signatures from the user using Signer functions.
-   *
-   * @param {*} needSigner - True if you need the signer, default false
-   * otherwise
-   */
   const getProviderOrSigner = async (needSigner = false) => {
-    // Connect to Metamask
-    // Since we store `web3Modal` as a reference, we need to access the `current` value to get access to the underlying object
     const provider = await web3ModalRef.current.connect();
     const web3Provider = new providers.Web3Provider(provider);
 
-    // If user is not connected to the Rinkeby network, let them know and throw an error
     const { chainId } = await web3Provider.getNetwork();
     if (chainId !== 152) {
       window.alert("Change the network to RedbellyDevNet");
@@ -258,32 +166,35 @@ export default function Home() {
     return web3Provider;
   };
 
-  // useEffects are used to react to changes in state of the website
-  // The array at the end of function call represents what state changes will trigger this effect
-  // In this case, whenever the value of `walletConnected` changes - this effect will be called
   useEffect(() => {
-    // if wallet is not connected, create a new instance of Web3Modal and connect the MetaMask wallet
-    if (!walletConnected) {
-      // Assign the Web3Modal class to the reference object by setting it's `current` value
-      // The `current` value is persisted throughout as long as this page is open
-      web3ModalRef.current = new Web3Modal({
-        network: "goerli",
-        providerOptions: {},
-        disableInjectedProvider: false,
-      });
-      connectWallet();
+    const initialize = async () => {
+      if (!walletConnected) {
+        web3ModalRef.current = new Web3Modal({
+          network: "RedbellyDevNet",
+          providerOptions: {},
+          disableInjectedProvider: false,
+        });
+        await connectWallet();
+      }
       getAmounts();
-    } else {
-      getAmounts();
+    };
+  
+    // Run the initialization when the component mounts
+    initialize();
+  
+    // Run the initialization again when walletConnected changes
+    // but only if it's true
+    if (walletConnected) {
+      initialize();
     }
   }, [walletConnected]);
+  
 
-  /*
-      renderButton: Returns a button based on the state of the dapp
-  */
+
+
+
       const renderButton = () => {
-        // If wallet is not connected, return a button which allows them to connect their wllet
-        // If we are currently waiting for something, return a loading button
+
     
         return (
           
@@ -313,7 +224,7 @@ export default function Home() {
                   </div>
                   </div>
     
-                  <button className="px-10 rounded-xl mb-5  border-1 px-14 text-white py-4 bg-gradient-to-r from-purple-500 via-red-500 to-yellow-500 hover:from-yellow-500 hover:to-purple-500 hover:via-red-500 ml-4 mt-3" onClick={_addLiquidity}>
+                  <button className=" rounded-xl mb-5  border-1 px-14 text-white py-4 bg-gradient-to-r from-purple-500 via-red-500 to-yellow-500 hover:from-yellow-500 hover:to-purple-500 hover:via-red-500 ml-4 mt-3" onClick={_addLiquidity}>
                     Add
                   </button>
                 </div>
@@ -374,31 +285,3 @@ export default function Home() {
   );
 }
 
-
-{/* <div>
-      <div className="min-h-screen flex flex-row justify-center items-center font-courier">
-        <div>
-          <h1 className={styles.title}>Welcome to Crypto Devs Exchange!</h1>
-          <div className={styles.description}>
-            Exchange Ethereum &#60;&#62; Crypto Dev Tokens
-          </div>
-          <div>
-            <button
-              className={styles.button}
-              onClick={() => {
-                setLiquidityTab(liquidityTab);
-              }}
-            >
-              Provide Liquidity
-            </button>
-          </div>
-          {renderButton()}
-        </div>
-        <div>
-        </div>
-      </div>
-
-      <footer className={styles.footer}>
-        Made with &#10084; by Crypto Devs
-      </footer>
-    </div> */}
